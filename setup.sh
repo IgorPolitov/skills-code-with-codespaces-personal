ece67875460c431decebc5c680818ad2b245c71f
IgorPolitov <igor.politov@cyara.com>
2024-06-11 06:02:46 +0000

    Adding setup.sh from the codespace!

---
+++ setup.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+# This script is used to update the package lists on the system.
+
+# Update the package lists
+sudo apt-get update
+
+# Install the 'sl' package
+sudo apt-get install sl
